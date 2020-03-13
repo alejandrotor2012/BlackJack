a16c07d8cbc7ee0578571ac1a6f3eac74dedb563
alejandrotor2012 <60798221+alejandrotor2012@users.noreply.github.com>
2020-03-12 22:23:24 -0400

    Add files via upload

--- BlackJack_Project.py
+++ BlackJack_Project.py
@@ -1,40 +1,100 @@
 import random
 
-#class Card_Deck:
+
+class Deck:
+       
+    card_suits = ['Hearts','Clover','Spades','Diamond']
+    card_values = ['A',2,3,4,5,6,7,8,9,10,'Jack','Queen','King']
+    card_deck = []
     
-    #quantity = 52
-    #card_value = ['A',1,2,3,4,5,6,7,8,9,10,10,10,10]
+    for suit in card_suits:
+        for value in card_values:
+            card_deck.append(str(value) + " of " + suit)
     
-    #def __init__(self):
+    random.shuffle(card_deck)
+    
+    def __init__(self):
+        pass
         
+    
+    def hand(self):
+        self.newhand = str(random.choice(Deck.card_deck)) #+ " AND " + str(random.choice(Dealer.card_deck))
+        return self.newhand
+
 
 class Player:
-
-    def __init__(self,name,funds):
+    
+    hand = ''
+    
+    def __init__(self,name='',funds=0):
+        name = input("What is your name? ")
+        funds = int(input("How much would you like to deposit in game credits? "))
+        
         self.name = name
         self.funds = funds
         
-    def action(self):
-        action = input("Would you like to hit or stay? (type either 'hit' or 'stay'): ")
-        if ("hit" in action.lower()):
-            bet = int(input("What is your bet? ")
+        
+    
+    def play(self):
+        
+        hit_or_stay = input("Would you like to hit or stay? (type either 'hit' or 'stay'): ")
+        
+        if ("hit" in hit_or_stay.lower()):
+            bet = int(input("What is your bet? "))
+            
             self.funds -= bet
             
+            return True
+        else:
+            return False
+        
+    def original_dealer_hand(self):
+        original_hand = CardDeck.hand()
+        Player.hand = original_hand
+        return self.hand
+
+    def new_dealer_hand(self):
+        new_hand = CardDeck.hand()
+        Player.hand += " & " + new_hand
+        return self.hand
+
 class Dealer:
        
-    quantity = 52
-    card_value = ['A',1,2,3,4,5,6,7,8,9,10,10,10,10]
     
+    hand = ''
     
     def __init__(self, funds):
         self.funds = funds
     
-    def hand(self):
-        self.newhand = str(random.choice(Dealer.card_value)) + str(random.choice(Dealer.card_value))
-        return self.newhand
-    
-Amy = Dealer()
+    def original_dealer_hand(self):
+        original_hand = CardDeck.hand()
+        Dealer.hand = original_hand
+        return self.hand
 
-print(Amy.hand)
-    
+    def new_dealer_hand(self):
+        new_hand = CardDeck.hand()
+        Dealer.hand += " & " + new_hand
+        return self.hand
+
+
+print("Welcome to Black Jack!")
+
+CardDeck = Deck()
+player1 = Player()
+house = Dealer(1000000)
+
+print("Dealer's hand is " + house.original_dealer_hand())
+print("Dealer's hand is " + house.new_dealer_hand())
+print("Player's hand is " + player1.original_dealer_hand())
+print("Player's hand is " + player1.new_dealer_hand())
+
+#print("Player's hand is " + player1.player_hand()+ " & " +  player1.player_hand())
+#New Note
+
+
+
+
+
+
+
     
