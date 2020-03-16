53f15deaa2a27eabef4c3e2af432413212845c9a
alejandrotor2012 <60798221+alejandrotor2012@users.noreply.github.com>
2020-03-16 15:11:34 -0400

    Add files via upload

--- BlackJack_Project(2).py
+++ BlackJack_Project(2).py
@@ -1,6 +1,5 @@
 import random
 
-
 class Deck:
        
     card_suits = ['Hearts','Clover','Spades','Diamond']
@@ -23,25 +22,28 @@ class Deck:
 
 
 class Player:
-    
-    hand = ''
+
     cards = 0
 
-    def __init__(self,name='',funds=0, wager =0, action=''):
+    def __init__(self,name='',funds=0, wager =0, action='',hand=''):
         self.name = input("What is your name? ")
         self.funds = int(input("How much would you like to deposit in game credits? "))
-        self.wager = int(input("What is your bet? "))
+        # self.wager = int(input("What is your bet? "))
         self.action = ''
         #self.name = name
         #self.funds = funds
-    
+
+    def reset(self):
+        self.action = ''
+        self.wager = 0
+
     def bet(self):
-        self.wager += int(input("What is your bet? "))
+        self.wager += int(input("\nWhat is your bet? "))
         #return self.wager
     
     def play(self):
         
-        hit_or_stay = input("Would you like to hit or stay? (type either 'hit' or 'stay'): ")
+        hit_or_stay = input("\nWould you like to hit or stay? (type either 'hit' or 'stay'): ")
         
         if ("hit" in hit_or_stay.lower()):
             #bet = int(input("What is your bet? "))
@@ -50,7 +52,7 @@ class Player:
             self.action = hit_or_stay.lower()
         elif ("stay" in hit_or_stay.lower()):
             self.action = hit_or_stay.lower()
-            return print("\nPlayer's hand is " + self.hand)
+            # return print("\nPlayer's hand is " + self.hand)
 
         else:
             print("\nYou have entered an incorrect value: Please type either 'hit' or 'stay'")
@@ -58,18 +60,16 @@ class Player:
         
     def original_player_hand(self):
         original_hand = CardDeck.hand() + " & " + CardDeck.hand()
-        Player.hand = original_hand
-        Player.cards = 1
+        self.hand = original_hand
+        self.cards = 1
         return print("\nPlayer's hand is " + self.hand)
 
     def new_player_hand(self):
         new_hand = CardDeck.hand()
-        Player.hand += " & " + new_hand
-        Player.cards += 1
+        self.hand += " & " + new_hand
+        self.cards += 1
         return print("\nPlayer's new hand is " + self.hand)
-        
 
-        
     def spoils(self, loot = 0):
         self.funds += loot
         
@@ -77,13 +77,14 @@ class Player:
         self.funds -= loot
 
 class Dealer:
-       
-    
-    hand = ''
+
     cards = 0
 
-    def __init__(self, funds):
+    def __init__(self, funds,hand=''):
         self.funds = funds
+
+    def reset(self):
+        self.hand = ''
 
     def play(self):
         
@@ -96,13 +97,13 @@ class Dealer:
     
     def original_dealer_hand(self):
         original_hand = CardDeck.hand() 
-        Dealer.hand = original_hand
+        self.hand = original_hand
         Dealer.cards = 1#saving
         return print("\nDealer's hand is " + self.hand)
 
     def new_dealer_hand(self):
         new_hand = CardDeck.hand()
-        Dealer.hand += " & " + new_hand
+        self.hand += " & " + new_hand
         Dealer.cards += 1
         return print("\nDealer's new hand is " + self.hand)
     
@@ -127,7 +128,7 @@ def winner():
     print("\nCongratulations " + player1.name + "! You have won this round!")
     player1.spoils(player1.wager)
     house.losses(player1.wager)
-    print(player1.funds)
+    print(player1.name + " funds: " + str(player1.funds))
     print(house.funds)
     
 def loser():
@@ -138,7 +139,7 @@ def loser():
     print(house.funds)
  
 def keep_playing(play):
-    user_response = input("\nWould you like to keep playing? [Enter either 'Yes' or 'No' ")    
+    user_response = input("\nWould you like to keep playing? [Enter either 'Yes' or 'No']: ")
     if 'y' in user_response.lower():
         play = True
     else:
@@ -146,44 +147,69 @@ def keep_playing(play):
     return play
     
 print("Welcome to Black Jack!")
+
+user_response = ""
+play = True
 CardDeck = Deck()
 player1 = Player()
 house = Dealer(1000000)
-user_response = ""
-play = True
 
 while play == True:
-    
+
     while True:
-        
+        house.reset()
+        player1.reset()
+        player1.bet()
+        # print("\n" + str(player1.funds))
+        # print("\n" + str(house.funds))
         house.original_dealer_hand()
         player1.original_player_hand()
-        
-        if (player1.hand == 21):
+
+        if (hand_converter(player1.hand) == 21):
             winner()
             play = keep_playing(play)
             break
-            
-        while (player1.action != "stay"): 
-            
+
+        while (player1.action != "stay"):
+
             if hand_converter(player1.hand) > 21:
                 loser()
-                play = keep_playing(play)
-                break                
+                #play = keep_playing(play)
+                break
             else:
                 player1.play()
-    
+
+        if hand_converter(player1.hand) > 21:
+            play = keep_playing(play)
+            break
+
         while hand_converter(house.hand) < hand_converter(player1.hand):
+            # # print(hand_converter(house.hand))
+            # # print(hand_converter(player1.hand))
+            # if hand_converter(house.hand) == hand_converter(player1.hand):
+            #     print("\nPUSH!")
+            #     break
+            # else:
             house.play()
-            
-        if ((hand_converter(player1.hand) > hand_converter(house.hand)) and (hand_converter(player1.hand) <= 21)) or (hand_converter(house.hand) > 21):
+
+        # if (hand_converter(house.hand)>21):
+        #     winner()
+        #     play = keep_playing(play)
+        #     break
+
+
+        # if hand_converter(house.hand) == hand_converter(player1.hand):
+        #     play = keep_playing(play)
+        #     break
+
+        if (hand_converter(player1.hand) > hand_converter(house.hand)) or (hand_converter(house.hand) > 21):
             winner()
             play = keep_playing(play)
             break
-            
+
         else:
             loser()
             play = keep_playing(play)
             break
-            
+
 #saving
